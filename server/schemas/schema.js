54247303b33839e96751b42ee0449d52a43682d3
n0IQ <varmamohit122@gmail.com>
2022-12-21 03:32:53 +0530

    Added Query and Mutations

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLSchema,
   GraphQLNonNull,
   GraphQLString,
+  GraphQLError,
 } = require('graphql');
 
 const User = require('../models/UserModel');
@@ -12,6 +13,26 @@ const Pin = require('../models/PinModel');
 
 const UserType = require('./UserSchema');
 const PinType = require('./PinSchema');
+const { default: mongoose } = require('mongoose');
+
+// check if the user exists
+const userExists = async (userId) => {
+  const user = await User.findById(userId);
+  if(!user) return false;
+  return user;
+}
+
+// check if the pin exists
+const pinExists = async (pinId) =>{
+  const pin = await Pin.findById(pinId);
+  if(!pin) return false;
+  return pin;
+}
+
+// check if id is valid
+const validId = (id) => {
+  return mongoose.Types.ObjectId.isValid(id);
+}
 
 // Query
 const RootQuery = new GraphQLObjectType({
@@ -27,8 +48,19 @@ const RootQuery = new GraphQLObjectType({
     // Get a Single User
     user: {
       type: UserType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLID) } 
+      },
+      async resolve(parent, args) {
+        // check if id is valid
+        if(!validId(args.id)) throw new GraphQLError('Invalid ID');
+        
+        // check if user exists
+        const user = await userExists(args.id);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+
         return User.findById(args.id);
       },
     },
@@ -42,9 +74,20 @@ const RootQuery = new GraphQLObjectType({
     // Get a Single Pin
     pin: {
       type: PinType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return Pin.findById(args.id);
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLID) } 
+      },
+      async resolve(parent, args) {
+        // check if id is valid
+        if(!validId(args.id)) throw new GraphQLError('Invalid ID');
+        
+        // check if pin exists
+        const pin = await pinExists(args.id);
+        if(!pin) {
+          throw new GraphQLError("Pin does not exist");
+        }
+
+        return pin;
       },
     },
   },
@@ -65,6 +108,7 @@ const Mutation = new GraphQLObjectType({
         password: { type: new GraphQLNonNull(GraphQLString) },
       },
       resolve(parent, args) {
+        // create a new user
         return User.create({
           firstName: args.firstName,
           lastName: args.lastName,
@@ -74,20 +118,61 @@ const Mutation = new GraphQLObjectType({
         });
       },
     },
+    // Update a User
+    updateUser : {
+      type : UserType,
+      args : {
+        id : { type : new GraphQLNonNull(GraphQLID) },
+        firstName : { type : GraphQLString },
+        lastName : {type : GraphQLString }
+      },
+      async resolve(parent, args) {
+        // check if id is valid
+        if(!validId(args.id)) throw new GraphQLError("Invalid User ID");
+
+        // check if user exists
+        const user = await userExists(args.id);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+        
+        // update the user
+        return await User.findByIdAndUpdate (
+          args.id,
+          {
+            $set:
+            {
+              firstName : args.firstName,
+              lastName : args.lastName,
+            }
+          },
+          { new : true },
+        );
+      },
+    },
     // Delete User
     deleteUser: {
       type: UserType,
       args: {
         id: { type: new GraphQLNonNull(GraphQLID) },
       },
-      resolve(parent, args) {
+      async resolve(parent, args) {
+        // check if id is valid
+        if(!validId(args.id)) {
+          throw new GraphQLError('Invalid User ID');
+        }
+
+        // check if the user exists
+        const user = await userExists(args.id);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+
         // delete all pins created by this user
-        Pin.find({ userId: args.id }).then((pins) => {
-          pins.forEach((pin) => {
-            pin.remove();
-          });
-        });
-        return User.findByIdAndRemove(args.id);
+        await Pin.deleteMany({ userId: args.id });
+        
+        // delete a user
+        return User.findByIdAndDelete(args.id);
       },
     },
     // Create a Pin
@@ -98,16 +183,39 @@ const Mutation = new GraphQLObjectType({
         imageUrl: { type: new GraphQLNonNull(GraphQLString) },
         description: { type: GraphQLString },
         link: { type: GraphQLString },
-        userName: { type: new GraphQLNonNull(GraphQLString) },
+        userId: { type: new GraphQLNonNull(GraphQLID) },
       },
-      resolve(parent, args) {
-        return Pin.create({
+      async resolve(parent, args) {
+        // check if id is valid
+        if(!validId(args.userId)) {
+          throw new GraphQLError('Invalid ID');
+        }
+
+        // check if the user exists
+        const user = await userExists(args.userId);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+
+        // create a pin
+        const pin = await Pin.create({
           title: args.title,
           imageUrl: args.imageUrl,
           description: args.description,
           link: args.link,
-          userName: args.userName,
+          userId: args.userId,
         });
+
+
+        // add the pin to the user's created pins
+        const createdPinsObj = [...user.createdPins, pin._id];
+        await User.findByIdAndUpdate(
+          args.userId,
+          { $set : { "createdPins" : createdPinsObj}},
+          {new: true},
+        );
+        
+        return pin;
       },
     },
     // Update a Pin
@@ -119,12 +227,36 @@ const Mutation = new GraphQLObjectType({
         imageUrl: { type: GraphQLString },
         description: { type: GraphQLString },
         link: { type: GraphQLString },
+        userId : { type : new GraphQLNonNull(GraphQLID)}
       },
-      resolve(parent, args) {
-        return Pin.findByIdAndUpdate(
+      async resolve(parent, args) {
+        // check if ids are valid
+        if(!validId(args.id)) throw new GraphQLError('Invalid Pin ID');
+        if(!validId(args.userId)) throw new GraphQLError('Invalid User ID');
+
+        // check if the user exists
+        const user = await userExists(args.userId);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+        
+        // check if the pin exists
+        const pin = await pinExists(args.id);
+        if(!pin) {
+          throw new GraphQLError("Pin does not exist");
+        }
+
+        // check if the user is the creator of the pin
+        if (pin.userId.valueOf() !== args.userId) {
+          throw new GraphQLError('User is not the Creator of this pin');
+        }
+
+        // update a pin
+        return await Pin.findByIdAndUpdate(
           args.id,
           {
-            $set: {
+            $set: 
+            {
               title: args.title,
               imageUrl: args.imageUrl,
               description: args.description,
@@ -140,19 +272,44 @@ const Mutation = new GraphQLObjectType({
       type: PinType,
       args: {
         id: { type: new GraphQLNonNull(GraphQLID) },
+        userId: { type: new GraphQLNonNull(GraphQLID) },
       },
-      resolve(parent, args) {
+      async resolve(parent, args) {
+        // check if Id is valid
+        if(!validId(args.id)) throw new GraphQLError('Invalid Pin ID');
+        else if(!validId(args.userId)) throw new GraphQLError('Invalid User ID');
+
+        // check if the user exists
+        const user = await userExists(args.userId);
+        if(!user) {
+          throw new GraphQLError("User does not exist");
+        }
+
+        // check if the pin exists
+        const pin = await pinExists(args.id);
+        if(!pin) {
+          throw new GraphQLError("Pin does not exist");
+        }
+
+        // check if the user is the creator of the pin
+        if (pin.userId.valueOf() !== args.userId) {
+          throw new GraphQLError('User is not the Creator of this pin');
+        }
+
         // remove this pin from the createdPins of the user
-        User.updateMany(
-          {},
-          {
-            $pullAll: {
-              createdPins: [args.id],
-            },
-          }
+        await User.findByIdAndUpdate(
+          args.userId,
+          { $pull : {createdPins : args.id} }
         );
+
+        // remove this pin from the savedPins of the users
+        await User.updateMany(
+          { savedPins: args.id },
+          { $pull: { savedPins: args.id } }
+        );
+      
         // delete this pin
-        return Pin.findByIdAndRemove(args.id);
+        return Pin.findByIdAndDelete(args.id);
       },
     },
   },
